703e79cd5c8f563820f6f06e68df8cd2724f5b8d
Chris Emerson <chris@cemerson.co.uk>
2015-02-14 23:50:30 +0000

    Add an extra button to open main settings, and flags to ensure the main activity dismisses the keyguard when it launches

--- app/src/main/java/uk/co/cemerson/flyst/MainActivity.java
+++ app/src/main/java/uk/co/cemerson/flyst/MainActivity.java
@@ -13,11 +13,28 @@ public class MainActivity extends ActionBarActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+        );
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Button adbWifiButton = (Button) findViewById(R.id.adbwifi_button);
+        adbWifiButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(Intent.ACTION_MAIN);
+                i.setClassName("com.rockolabs.adbkonnect", "com.rockolabs.adbkonnect.AdbKonnectActivity");
+
+                startActivity(i);
+            }
+        });
 
         Button settingsButton = (Button) findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(new View.OnClickListener()
@@ -26,6 +43,7 @@ public class MainActivity extends ActionBarActivity
             public void onClick(View v)
             {
                 Intent i = new Intent(Settings.ACTION_SETTINGS);
+
                 startActivity(i);
             }
         });
