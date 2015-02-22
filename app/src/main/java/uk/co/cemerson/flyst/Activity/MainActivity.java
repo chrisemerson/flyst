ddaa209323c86598b63802aa774832fc64df205c
Chris Emerson <chris@cemerson.co.uk>
2015-02-22 00:32:34 +0000

    Change current date into text rather than button, and create a MainActivity class to serve as the main screen for operations

---
+++ app/src/main/java/uk/co/cemerson/flyst/Activity/MainActivity.java
@@ -0,0 +1,12 @@
+package uk.co.cemerson.flyst.Activity;
+
+import android.os.Bundle;
+
+public class MainActivity extends FlystActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
