1908d76ac186866e5c9b432027dd1a570b48a99a
Chris Emerson <chris@cemerson.co.uk>
2015-03-11 00:36:19 +0000

    Add Upcoming Tasks panel that is mostly hidden off screen, add callback methods for notifying of new events and tidy up

--- app/src/main/java/uk/co/cemerson/flyst/activity/FlystLayoutActivity.java
+++ app/src/main/java/uk/co/cemerson/flyst/activity/FlystLayoutActivity.java
@@ -1,14 +1,18 @@
 package uk.co.cemerson.flyst.activity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import uk.co.cemerson.flyst.R;
 import uk.co.cemerson.flyst.fragment.StatusFragment;
 import uk.co.cemerson.flyst.fragment.UpcomingTasksFragment;
 
-abstract public class FlystLayoutActivity extends FlystBaseActivity
+abstract public class FlystLayoutActivity extends FlystBaseActivity implements
+    UpcomingTasksFragment.UpcomingTasksCallbackListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,10 +22,36 @@ abstract public class FlystLayoutActivity extends FlystBaseActivity
         addFragment(R.id.statusFragmentContainer, new StatusFragment());
         addFragment(R.id.upcomingTasksFragmentContainer, new UpcomingTasksFragment());
 
-        addUpcomingEventsSliderClickListener();
+        initialiseUpcomingTasksFragmentLayout();
     }
 
-    private void addUpcomingEventsSliderClickListener()
+    private void initialiseUpcomingTasksFragmentLayout()
+    {
+        initUpcomingTasksSlidingPanelLayoutParams();
+        initUpcomingEventsSliderClickListener();
+    }
+
+    private void initUpcomingTasksSlidingPanelLayoutParams()
+    {
+        LinearLayout upcomingTasksSlidingPanel = (LinearLayout) findViewById(R.id.upcomingTasksSlidingPanel);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) upcomingTasksSlidingPanel.getLayoutParams();
+
+        params.setMargins(0, 0, getUpcomingTasksSlidingPanelRequiredOffset(), 0);
+
+        upcomingTasksSlidingPanel.setLayoutParams(params);
+    }
+
+    private int getUpcomingTasksSlidingPanelRequiredOffset()
+    {
+        Resources resources = getResources();
+
+        float upcomingTasksOffsetInDIP = resources.getDimension(R.dimen.upcoming_tasks_panel_width) - resources.getDimension(R.dimen.clickable_bar_width);
+        float scale = resources.getDisplayMetrics().density;
+
+        return 0 - Math.round(upcomingTasksOffsetInDIP * scale);
+    }
+
+    private void initUpcomingEventsSliderClickListener()
     {
         ImageButton upcomingEventsSliderButton = (ImageButton) findViewById(R.id.upcoming_events_slider_button);
 
@@ -30,9 +60,15 @@ abstract public class FlystLayoutActivity extends FlystBaseActivity
             @Override
             public void onClick(View v)
             {
-                //Slide out UpcomingEvents Fragment here...
+                //Toggle UpcomingTasks sliding panel here...
             }
         });
+    }
+
+    @Override
+    public void newEvents()
+    {
+        //Flash upcoming tasks button
     }
 
     @Override
--- app/src/main/java/uk/co/cemerson/flyst/fragment/UpcomingTasksFragment.java
+++ app/src/main/java/uk/co/cemerson/flyst/fragment/UpcomingTasksFragment.java
@@ -1,5 +1,6 @@
 package uk.co.cemerson.flyst.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,15 +11,41 @@ import uk.co.cemerson.flyst.R;
 
 public class UpcomingTasksFragment extends TimeTickReceivingFragment
 {
+    private UpcomingTasksCallbackListener callbackListener = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.fragment_upcomingtasks, container, false);
     }
 
+    public interface UpcomingTasksCallbackListener
+    {
+        abstract public void newEvents();
+    }
+
     @Override
     protected void onTimeTick()
     {
-        //noop currently
+        if (callbackListener != null) {
+            //Call new events for testing, just for the hell of it
+            callbackListener.newEvents();
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+
+        callbackListener = (UpcomingTasksCallbackListener) activity;
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+
+        callbackListener = null;
     }
 }
