2838072a094a44c1b69424f7c169298aa9b87aa7
Chris Emerson <chris@cemerson.co.uk>
2015-03-28 16:21:18 +0000

    Upcoming Task providers

---
+++ app/src/main/java/uk/co/cemerson/flyst/upcomingtaskprovider/NewWinchDriverRequiredUpcomingTaskProvider.java
@@ -0,0 +1,5 @@
+package uk.co.cemerson.flyst.upcomingtaskprovider;
+
+public class NewWinchDriverRequiredUpcomingTaskProvider implements UpcomingTaskProvider
+{
+}
--- app/src/main/java/uk/co/cemerson/flyst/upcomingtaskprovider/WinchDriverRequired.java
+++
@@ -1,5 +0,0 @@
-package uk.co.cemerson.flyst.upcomingtaskprovider;
-
-public class WinchDriverRequired implements UpcomingTaskProvider
-{
-}
